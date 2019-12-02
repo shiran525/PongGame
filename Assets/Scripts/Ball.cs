56f85f72297f11cf67f459b22b90e2380d64eac4
Shiran <shiranavr226@gmail.com>
2019-12-02 18:03:14 +0200

    my project

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float startX = Random.Range(0, 2) == 0 ? -1 : 1;
+        float startY = Random.Range(0, 2) == 0 ? -1 : 1;
+
+        GetComponent<Rigidbody>().velocity = new Vector3(speed * startX, speed * startY, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((GetComponent<Rigidbody>().position.x>13)||( GetComponent<Rigidbody>().position.x< -13)){
+            sceneLoader(0);
+        }
+        speed += 0.1f;
+        
+
+    }
+    public void sceneLoader(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ Assets/Scripts/Racket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+    public bool isRack1;
+    public float speed=5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRack1)
+        {
+            transform.Translate(0, Input.GetAxis("Vertical") * speed * Time.deltaTime,0);
+        }
+        else
+        {
+            transform.Translate(0, Input.GetAxis("Vertical2") * speed * Time.deltaTime, 0);
+        }
+    }
+}
